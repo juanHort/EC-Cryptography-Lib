716391129e0b4fdeaa36aa8ab9406de2e2f22f59
Juan Horta <jhorta89@hotmail.com>
2023-09-21 20:40:40 -0600

    Add needes structures for EC basic operations

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,51 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use num_bigint::{BigUint};
+
+struct Point{
+    x: BigUint,
+    y: BigUint
+}
+struct EllipticCurve{
+    a: BigUint,
+    b: BigUint,
+    p: BigUint
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl EllipticCurve{
+    fn add(c: &Point, d: &Point) -> Point{
+        todo!();
     }
+
+    fn double(c: &Point) -> Point{
+        todo!();
+    }
+    fn scalar_mul(c: &Point, d: &BigUint) -> Point{
+        // addition/doubling algorithm
+        todo!();
+    }
+
+}
+
+struct FiniteField {}
+
+impl FiniteField{
+    fn addition(c: &BigUint, d: &BigUint, p: &BigUint) -> BigUint{
+        // c + d = r mod p
+        todo!();
+    }
+
+    fn multtiplication(c: &BigUint, d: &BigUint, p: &BigUint) -> BigUint{
+        // c * d = r mod p
+        todo!();
+    }
+
+    fn inverse_addition(c: &BigUint, p: &BigUint) -> BigUint{
+        // -c mod p
+        todo!();
+    }
+
+    fn inverse_multiplication(c: &BigUint, p: &BigUint) -> BigUint{
+        // c^(-1) mod p
+        todo!();
+    }
+
 }
