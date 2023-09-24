dabb3beb8230c485d0c049d8ed665db9eb198729
Juan Horta <jhorta89@hotmail.com>
2023-09-23 21:01:22 -0600

    Add operations for the Finite field struct

--- src/lib.rs
+++ src/lib.rs
@@ -28,24 +28,116 @@ impl EllipticCurve{
 struct FiniteField {}
 
 impl FiniteField{
-    fn addition(c: &BigUint, d: &BigUint, p: &BigUint) -> BigUint{
+    fn add(c: &BigUint, d: &BigUint, p: &BigUint) -> BigUint{
         // c + d = r mod p
-        todo!();
+        let r: BigUint = c + d;
+        r.modpow(&BigUint::from(1u32), p)
     }
 
-    fn multtiplication(c: &BigUint, d: &BigUint, p: &BigUint) -> BigUint{
+    fn multiplication(c: &BigUint, d: &BigUint, p: &BigUint) -> BigUint{
         // c * d = r mod p
-        todo!();
+        let r: BigUint = c * d;
+        r.modpow(&BigUint::from(1u32), p)
     }
 
     fn inverse_addition(c: &BigUint, p: &BigUint) -> BigUint{
         // -c mod p
-        todo!();
+        assert!(
+            c < p,
+            "Number c: {} is bigger or equal than p: {}", *c , *p
+        );
+        p - c
     }
 
     fn inverse_multiplication(c: &BigUint, p: &BigUint) -> BigUint{
+        // TODO: this function only is valid for a p prime
         // c^(-1) mod p
-        todo!();
+        c.modpow(&(p - BigUint::from(2u32)), p)
+    }
+
+}
+
+#[cfg(test)]
+mod test{
+    use super::*;
+
+    #[test]
+    fn test_add_1(){
+        let c: BigUint = BigUint::from(4u32);
+        let d: BigUint = BigUint::from(10u32);
+        let p: BigUint = BigUint::from(11u32);
+
+        let r: BigUint = FiniteField::add(&c, &d, &p);
+        assert_eq!(r, BigUint::from(3u32));
+    }
+
+    #[test]
+    fn test_add_2(){
+        let c: BigUint = BigUint::from(4u32);
+        let d: BigUint = BigUint::from(10u32);
+        let p: BigUint = BigUint::from(31u32);
+
+        let r: BigUint = FiniteField::add(&c, &d, &p);
+        assert_eq!(r, BigUint::from(14u32));
+    }
+
+    #[test]
+    fn test_multiplication_1(){
+        let c: BigUint = BigUint::from(4u32);
+        let d: BigUint = BigUint::from(10u32);
+        let p: BigUint = BigUint::from(11u32);
+
+        let r: BigUint = FiniteField::multiplication(&c, &d, &p);
+        assert_eq!(r, BigUint::from(7u32));
+    }
+
+    #[test]
+    fn test_multiplication_2(){
+        let c: BigUint = BigUint::from(4u32);
+        let d: BigUint = BigUint::from(10u32);
+        let p: BigUint = BigUint::from(41u32);
+
+        let r: BigUint = FiniteField::multiplication(&c, &d, &p);
+        assert_eq!(r, BigUint::from(40u32));
+    }
+
+    #[test]
+    fn test_inverse_addition_1(){
+        let c: BigUint = BigUint::from(4u32);
+        let p: BigUint = BigUint::from(51u32);
+
+        let r: BigUint = FiniteField::inverse_addition(&c, &p);
+        assert_eq!(r, BigUint::from(47u32));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_inverse_addition_2(){
+        let c: BigUint = BigUint::from(52u32);
+        let p: BigUint = BigUint::from(51u32);
+
+        FiniteField::inverse_addition(&c, &p);
+    }
+
+    #[test]
+    fn test_inverse_addition_identity(){
+        let c: BigUint = BigUint::from(4u32);
+        let p: BigUint = BigUint::from(51u32);
+
+        let c_inverse: BigUint = FiniteField::inverse_addition(&c, &p);
+        assert_eq!(c_inverse, BigUint::from(47u32));
+        assert_eq!(FiniteField::add(&c, &c_inverse, &p), BigUint::from(0u32));
+    }
+
+    #[test]
+    fn test_inverse_multiplication(){
+        let c: BigUint = BigUint::from(4u32);
+        let p: BigUint = BigUint::from(11u32);
+
+        let c_inverse: BigUint = FiniteField::inverse_multiplication(&c, &p);
+
+        assert_eq!(c_inverse, BigUint::from(3u32));
+        assert_eq!(FiniteField::multiplication(&c, &c_inverse, &p), BigUint::from(1u32))
     }
 
 }
